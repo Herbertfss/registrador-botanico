ea124492b5500c7320d4e931971c7c58d2fd4522
Herbertfss <herbertsantows@gmail.com>
2026-05-05 20:06:08 -0300

    fix: update environment variables and remove unused files

--- backend/prisma/migrations/20260505212852_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "PlantIdentification" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "imageUrl" TEXT NOT NULL,
-    "identifiedBy" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
